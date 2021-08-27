c210afcfd9826a9b9d4edf21113aa2cc4080cb14
Santi <santiagohenn@gmail.com>
2021-08-27 16:01:13 -0300

    Added some more properties. Modified CPGD to take configuration on said properties.

--- src/analysis/CPGD.java
+++ src/analysis/CPGD.java
@@ -21,6 +21,17 @@ public class CPGD {
     static final String SEARCH_DATE = (String) properties.get("search_date");
     static final double TIME_STEP = Double.parseDouble((String) properties.get("time_step"));
     static final double VISIBILITY_THRESHOLD = Double.parseDouble((String) properties.get("visibility_threshold"));
+    static final double MAX_MCG = Double.parseDouble((String) properties.get("max_mcg"));
+    static final double MAX_LAT = Double.parseDouble((String) properties.get("max_lat"));
+    static final int MIN_PLANES = Integer.parseInt((String) properties.get("min_planes"));
+    static final int MAX_PLANES = Integer.parseInt((String) properties.get("max_planes"));
+    static final int MIN_SATS_IN_PLANE = Integer.parseInt((String) properties.get("min_sats_in_plane"));
+    static final int MAX_SATS_IN_PLANE = Integer.parseInt((String) properties.get("max_sats_in_plane"));
+    static final double MAX_INCLINATION = Double.parseDouble((String) properties.get("max_inclination"));
+    static final double INCLINATION_STEP = Double.parseDouble((String) properties.get("inclination_step"));
+    static final double SEMI_MAJOR_AXIS = Double.parseDouble((String) properties.get("semi_major_axis"));
+    static final double ECCENTRICITY = Double.parseDouble((String) properties.get("eccentricity"));
+    static final double PERIGEE_ARGUMENT = Double.parseDouble((String) properties.get("perigee_argument"));
 
     static final String CSV_EXTENSION = ".csv";
     static final String LOG_EXTENSION = ".log";
@@ -33,32 +44,17 @@ public class CPGD {
 
         multiGatewayAnalysis.setIncludeCoverageGaps(true);
 
-        // Maximum Coverage Gap in minutes
-        double maxMCG = 120;
-
-        // Maximum coverage latitude
-        double latMax = 80;
-
-        // Minimum and maximum values for scenario parameters
-        int minPlanes = 3;
-        int maxPlanes = 4;
-        int minSatsInPlane = 3;
-        int maxSatsInPlane = 4;
-        double minInclination = getInclination(getLambdaMax(new Satellite(START_DATE,
-                7000672.074930292, 1.478E-4, 45,
-                250, 80, 0), 5), latMax);
+        double minInclination = getInclination(SEMI_MAJOR_AXIS, ECCENTRICITY, VISIBILITY_THRESHOLD, MAX_LAT);
         minInclination = Math.round(minInclination * 100.0) / 100.0;
-        double maxInclination = 90;
-        double inclinationStep = 5;
 
         statsLogger.add("Starting analysis at " + Utils.unix2stamp(System.currentTimeMillis()));
         statsLogger.add("Scenario start: " + START_DATE + " - Scenario end: " + END_DATE);
-        statsLogger.add("Target MCG: " + maxMCG + " - Maximum latitude band: " + latMax
+        statsLogger.add("Target MCG: " + MAX_MCG + " - Maximum latitude band: " + MAX_LAT
                 + " Degrees - complexity 0 search date " + SEARCH_DATE);
-        statsLogger.add("Minimum number of planes: " + minPlanes + " - Maximum number of planes: " + maxPlanes);
-        statsLogger.add("Minimum sats per plane: " + minSatsInPlane + " - Maximum sats per planes: " + maxSatsInPlane);
-        statsLogger.add("Minimum inclination: " + minInclination + " - Maximum inclination: " + maxInclination);
-        statsLogger.add("Inclination step: " + inclinationStep + " Degrees");
+        statsLogger.add("Minimum number of planes: " + MIN_PLANES + " - Maximum number of planes: " + MAX_PLANES);
+        statsLogger.add("Minimum sats per plane: " + MIN_SATS_IN_PLANE + " - Maximum sats per planes: " + MAX_SATS_IN_PLANE);
+        statsLogger.add("Minimum inclination: " + minInclination + " - Maximum inclination: " + MAX_INCLINATION);
+        statsLogger.add("Inclination step: " + INCLINATION_STEP + " Degrees");
         statsLogger.add("====================================================================== PROGRESS " +
                 "======================================================================");
 
@@ -66,8 +62,8 @@ public class CPGD {
         int[] discarded = new int[5];
 
         // Initial solution
-        int currentPlanes = minPlanes;
-        int currentSatsInPlane = minSatsInPlane;
+        int currentPlanes = MIN_PLANES;
+        int currentSatsInPlane = MIN_SATS_IN_PLANE;
         double currentInclination = minInclination;
 
         boolean go = true;
@@ -83,7 +79,7 @@ public class CPGD {
         List<Satellite> satellites = new ArrayList<>();
 
         // grid resolution longitude - wise
-        double longitudeResolution = maxMCG * 0.25;
+        double longitudeResolution = MAX_MCG * 0.25;
 
         // Number of grid points according to the grid resolution
         int nFacilities = (int) Math.round(360 / longitudeResolution);
@@ -101,8 +97,8 @@ public class CPGD {
             int facId = 0;
             for (int fac = 0; fac < nFacilities; fac++) {
                 devices.add(new Device(facId++, 0.0, fac * longitudeResolution, 0.0));
-                devices.add(new Device(facId++, latMax / 2, fac * longitudeResolution, 0.0));
-                devices.add(new Device(facId++, latMax, fac * longitudeResolution, 0.0));
+                devices.add(new Device(facId++, MAX_LAT / 2, fac * longitudeResolution, 0.0));
+                devices.add(new Device(facId++, MAX_LAT, fac * longitudeResolution, 0.0));
             }
 
             double planePhase = 360.0 / currentPlanes;
@@ -114,8 +110,8 @@ public class CPGD {
             int satId = 0;
             for (int plane = 0; plane < currentPlanes; plane++) {   // Plane
                 for (int sat = 0; sat < currentSatsInPlane; sat++) {    // Satellites
-                    satellites.add(new Satellite(satId++, START_DATE, 7000000.0,
-                            1.0E-4, currentInclination, plane * planePhase, 0,
+                    satellites.add(new Satellite(satId++, START_DATE, SEMI_MAJOR_AXIS,
+                            ECCENTRICITY, currentInclination, plane * planePhase, PERIGEE_ARGUMENT,
                             sat * satsPhase));  // - plane * (planePhase / currentSatsInPlane)
                 }
             }
@@ -140,7 +136,7 @@ public class CPGD {
 
             List<Double> exploredLatitudes = new ArrayList<>();
 
-            if (multiGatewayAnalysis.getMaxMCGMinutes() <= maxMCG) { // If the MCG requirement is met, increase complexity
+            if (multiGatewayAnalysis.getMaxMCGMinutes() <= MAX_MCG) { // If the MCG requirement is met, increase complexity
 
                 // Increase scenario time
                 multiGatewayAnalysis.setScenarioParams(START_DATE, END_DATE, TIME_STEP, VISIBILITY_THRESHOLD);
@@ -148,13 +144,13 @@ public class CPGD {
                 for (complexity = 1; complexity <= 4; complexity++) {
 
                     double latitudeResolution = Math.pow(2, complexity);
-                    double step = latMax / latitudeResolution;
-                    double lastStep = latMax - step;
+                    double step = MAX_LAT / latitudeResolution;
+                    double lastStep = MAX_LAT - step;
                     double firstStep;
 
                     if (complexity == 1) {
                         firstStep = 0;
-                        lastStep = latMax;
+                        lastStep = MAX_LAT;
                     } else {
                         firstStep = step;
                     }
@@ -187,7 +183,7 @@ public class CPGD {
                             + multiGatewayAnalysis.getLastSimTime() + " ms.");
 
                     // If the requirement is not met after increasing complexity, break the loop
-                    if (multiGatewayAnalysis.getMaxMCGMinutes() > maxMCG) {
+                    if (multiGatewayAnalysis.getMaxMCGMinutes() > MAX_MCG) {
                         break;
                     }
 
@@ -195,7 +191,7 @@ public class CPGD {
             }
 
             // Add solution found
-            if (multiGatewayAnalysis.getMaxMCGMinutes() <= maxMCG) {
+            if (multiGatewayAnalysis.getMaxMCGMinutes() <= MAX_MCG) {
                 log("SOLUTION!: " + currentPlanes + " planes with " + currentSatsInPlane
                         + " satellites at " + currentInclination + " degrees. MCG: " + multiGatewayAnalysis.getMaxMCGMinutes());
 
@@ -211,21 +207,21 @@ public class CPGD {
             }
 
             // Here we perform the movement towards another solutions
-            currentInclination += inclinationStep;
+            currentInclination += INCLINATION_STEP;
             currentInclination = Math.round(currentInclination * 100.0) / 100.0;
 
-            if (currentInclination > maxInclination || solutionFound) {
+            if (currentInclination > MAX_INCLINATION || solutionFound) {
 
                 solutionFound = false;
                 currentInclination = minInclination;
                 currentSatsInPlane++;
 
-                if (currentSatsInPlane > maxSatsInPlane) {
-                    currentSatsInPlane = minSatsInPlane;
+                if (currentSatsInPlane > MAX_SATS_IN_PLANE) {
+                    currentSatsInPlane = MIN_SATS_IN_PLANE;
                     currentPlanes++;
                 }
 
-                if (currentPlanes > maxPlanes) {
+                if (currentPlanes > MAX_PLANES) {
                     go = false;
                 }
 
@@ -254,12 +250,11 @@ public class CPGD {
      * This method returns the maximum Lambda, which is defined as the maximum Earth Central Angle or
      * half of a satellite's "cone FOV" over the surface of the Earth.
      **/
-    public static double getLambdaMax(Satellite satellite, double th) {
+    public static double getLambdaMax(double semiMajorAxis, double eccentricity, double visibilityThreshold) {
 
-        double hMax = ((1 + satellite.getElements().getEccentricity()) *
-                satellite.getElements().getSemiMajorAxis()) - Utils.EARTH_RADIUS;
-        double etaMax = Math.asin((Utils.EARTH_RADIUS * Math.cos(Math.toRadians(th))) / (Utils.EARTH_RADIUS + hMax));
-        return 90 - th - Math.toDegrees(etaMax);
+        double hMax = ((1 + eccentricity) * semiMajorAxis) - Utils.EARTH_RADIUS;
+        double etaMax = Math.asin((Utils.EARTH_RADIUS * Math.cos(Math.toRadians(visibilityThreshold))) / (Utils.EARTH_RADIUS + hMax));
+        return 90 - visibilityThreshold - Math.toDegrees(etaMax);
 
     }
 
@@ -267,11 +262,11 @@ public class CPGD {
      * This is a numerical method to obtain the inclination at which the percentage of coverage at the maximum
      * latitude equals the percentage of coverage at the equator
      **/
-    public static double getInclination(double lambda, double latMax) {
+    public static double getInclination(double semiMajorAxis, double eccentricity, double visibilityThreshold, double latMax) {
 
         double inc = 55, pLo, pLm; // inclination, percentage at zero latitude, percentage at maximum latitude
 
-        double lam = Math.toRadians(lambda);
+        double lam = Math.toRadians(getLambdaMax(semiMajorAxis, eccentricity, visibilityThreshold));
         double lat = Math.toRadians(latMax);
 
         double inc0 = 1;
